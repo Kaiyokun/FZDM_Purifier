73e05eb82f3f48e4dc885e2edff17f144149af66
Kaiyokun <Kaiyokun@live.com>
2018-05-01 12:02:26 +0800

    Mirror bugs fixed.

--- FZDM_Purifier.user.js
+++ FZDM_Purifier.user.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name         FZDM_Purifier
 // @namespace    https://github.com/Kaiyokun/FZDM_Purifier/
-// @version      0.1
+// @version      0.2
 // @author       Kaiyokun
 // @description  Purify your reading experience of manga at https://manhua.fzdm.com/
 // @updateURL    https://raw.githubusercontent.com/Kaiyokun/FZDM_Purifier/master/FZDM_Purifier.user.js
 // @downloadURL  https://raw.githubusercontent.com/Kaiyokun/FZDM_Purifier/master/FZDM_Purifier.user.js
 // @supportURL   https://github.com/Kaiyokun/FZDM_Purifier/issues
-// @match        https://manhua.fzdm.com/*/*
+// @match        https://manhua.fzdm.com/*/*/
 // @require      http://code.jquery.com/jquery-latest.js
 // @grant        none
 // ==/UserScript==
@@ -29,8 +29,9 @@ function loadNextPage($imgContainer) {
         () => {
             // 没有下一页了
             if ($div.children().length < 2) {
-                $imgContainer.find('img:last')
-                    .wrap($div.find('a:last').empty());
+                // 点击最后一页进入下一话，或者在最后一话返回章节列表
+                $imgContainer.find('img:last').wrap($('<a href="' +
+                    ($div.find('a:last').attr('href') || '..') + '" />'));
                 return;
             }
 
@@ -52,15 +53,24 @@ function loadNextPage($imgContainer) {
 }
 
 (function showChapter() {
+    // 获取第一页漫画 URL
+    var firstPageMangaUrl = $('img#mhpic').attr('src');
     // 获取页面导航栏
-    var pageCollection = $('div.navigation>a:not(.pure-button-primary)');
+    var $pageCollection = $('div.navigation>a.button-success~' +
+        'a:not(.pure-button-primary)');
+
     // 清空原有页面并阻止未执行完的 document.write 影响新页面
     document.write('<clear />');
 
-    // 居中所有漫画
-    var $center = $('<center />').appendTo('body');
-    // 添加第一页到最后一页的漫画
-    pageCollection.each((_, a) => {
+    // 居中所有漫画并添加第一页
+    var $center = $('<center />').append($('<img />', {
+        src: firstPageMangaUrl,
+        alt: '第1页',
+        pageUrl: location.href
+    })).append('<br /><br />').appendTo('body');
+
+    // 添加第二页到最后一页的漫画
+    $pageCollection.each((_, a) => {
         // 异步加载漫画
         loadMangaUrl($('<img />', {
             alt: '第' + a.innerText.replace(/[^\d]/g, '') + '页',
@@ -71,6 +81,9 @@ function loadNextPage($imgContainer) {
         $center.append('<br /><br />');
     });
 
-    // 访问最后一页
+    // 递归添加后续页
     loadNextPage($center);
+
+    // 使图片宽度保持与页面同宽
+    $(window).resize(() => { $('img').css('width', $(window).width()); });
 })();
